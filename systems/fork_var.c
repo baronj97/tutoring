a87182403743cf178fdd4d61a97b7e726c1ea03c
baronj97 <jab644@drexel.edu>
2020-07-11 10:38:08 -0400

    Add fork loop example

---
+++ systems/fork_var.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main() {
+    int i;
+
+    for (i =0; i < 3; i++){
+        fork();
+        printf("Parent = [%d], Child = [%d] i=%d\n", getppid(), getpid(), i); 
+    }
+    printf("Parent = [%d],  Child = [%d] finished\n", getppid(), getpid());
+
+    return 0;
+}
