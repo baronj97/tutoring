1dc84683696f39abd58bbb219f72a435fc5ff93d
baronj97 <jab644@drexel.edu>
2020-07-16 15:45:25 -0400

    Add comments for signal handler example

--- systems/signal_handler.c
+++ systems/signal_handler.c
@@ -2,15 +2,16 @@
 #include <signal.h>
 #include <unistd.h>
 
+/* Signal handler to catch various signals and print out its information */
 void signal_handler(int sig)
 {
-    printf("Caught signal %d = %s\n", sig, sys_signame[sig]);
+    printf("Caught signal %d = %s\n", sig, sys_signame[sig]); /* sys_signame is an array that returns the signal's name */
 }
 
 int main()
 {
-    signal(SIGINT, signal_handler);
-    while (1)
+    signal(SIGINT, signal_handler); /* Register the signal (SIGINT) to the signal handler */
+    while (1)                       /* Loop forever */
     {
         printf("Hello, world!\n");
         sleep(1);
